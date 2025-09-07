ba4d80ed304ab0954af89b1238f65e7cc8cdeec3
Rin <info@rin-m.site>
2025-09-07 19:46:22 +0900

    feat: add favicon.ico and configure metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
 	title: 'Rin Teaholic - Portfolio',
 	description: 'Web Developer Portfolio - Rin Teaholic',
+	icons: {
+		icon: '/favicon.ico',
+	},
 }
 
 export default function RootLayout({
